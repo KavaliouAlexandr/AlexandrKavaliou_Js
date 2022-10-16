8c9e8c5f379c14419abc602d956ab2fb0db695d8
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-10-16 19:59:08 +0200

    PrevNextbtns

--- Javascript/Slider/Slider.js
+++ Javascript/Slider/Slider.js
@@ -1,46 +1,48 @@
-// const main = document.querySelector('main')
+let licznik = 0 
+const main = document.querySelector('main')
+const slides = document.querySelector('.slides')
+
+
+document.querySelector('.next').addEventListener('click', () => {
+    licznik = licznik + 443;
+    if (licznik > 1329)
+    {
+        licznik = 0;
+    }
+
+    slides.style.left = -licznik + 'px';
+})
+
+document.querySelector('.prev').addEventListener('click', () => {
+    licznik = licznik - 443;
+    if (licznik < 0)
+    {
+        licznik = 1329;
+    }
+
+    slides.style.left = -licznik + 'px';
+})
+
+
+
 // const timeoutRef = setTimeout( 
 //     () => {
 //         main.innerHTML='From setTimeout'
 //     },
 //     2000
 // )
-// let licznik = 0 
+
 // const intervalRef = setInterval( 
 //     () => {
 //         main.innerHTML='From interval' + licznik++
 //     },
 //     4000
 // )
-
 // // kasujemy setInterval
 // clearInterval(intervalRef)
 
 // // kasujemy setTimeout
 // clearTimeout(intervalRef)
 
-const main = document.querySelector('main')
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.slider').forEach((el) => {
-         new ISlider('.slider', {
-        
-        loop: false,
-        swipe: false
-        });
-    })
-
-    
-    
-})
-
-this._elsItem.forEach((el, index) => {
-    el.dataset.index = index;
-    el.dataset.order = index;
-    el.dataset.translate = 0;
-  });
-
-this._intervalID = setInterval(() => {
-    this._direction = 'next';
-    this._move();
-}, this._config.interval)
+// window.requestAnimationFrame
