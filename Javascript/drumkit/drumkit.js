27cf1901e1f58b903c469b36b68478f75227cd62
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-11-01 15:39:25 +0100

    Drumkit

--- Javascript/drumkit/drumkit.js
+++ Javascript/drumkit/drumkit.js
@@ -1,15 +1,35 @@
+// document.addEventListener('keypress', onKeyPress)
+
+// function onKeyPress(event) {
+//     const key = event.key
+//     // ktory dzwiek w zaleznosci od klawisza
+//     const sound = 'clap'
+//     playSound(sound)
+// }
+
+
+// function playSound(sound) {
+//     const audioTag = document.querySelector('#' + sound)
+//     audioTag.currentTime = 0 
+//     audioTag.play()
+// }
+
 document.addEventListener('keypress', onKeyPress)
 
-function onKeyPress(event) {
-    const key = event.key
-    // ktory dzwiek w zaleznosci od klawisza
-    const sound = 'clap'
-    playSound(sound)
+const KeyToSound = {
+    'a': document.querySelector('#s1'),
+    's': document.querySelector('#s2'),
+    'd': document.querySelector('#s3'),
+    'f': document.querySelector('#s4'),
+    'g': document.querySelector('#s5'),
+    'h': document.querySelector('#s6')
 }
 
-
+function onKeyPress(event) {
+    const sound = KeyToSound[event.key]
+    playSound(sound)
+}
 function playSound(sound) {
-    const audioTag = document.querySelector('#' + sound)
-    audioTag.currentTime = 0 
-    audioTag.play()
+    sound.currentTime = 0
+    sound.play()
 }
