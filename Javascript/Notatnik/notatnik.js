0fe8fd51a9def724ea4c5d4e362cafd37f7e29fd
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-11-09 23:19:39 +0100

    NotePinUpdate

--- Javascript/Notatnik/notatnik.js
+++ Javascript/Notatnik/notatnik.js
@@ -12,6 +12,7 @@ btnadd.addEventListener('click', () => {
     localStorage.setItem(`title${n}`, document.querySelector('#title').value);
     localStorage.setItem(`text${n}`, document.querySelector('#text').value);
     localStorage.setItem(`color${n}`, document.querySelector('#color').value);
+    localStorage.setItem(`pin${n}`, document.querySelector('#pin').checked);
     localStorage.setItem(`date${n}`, new Date().toISOString());
     console.log(localStorage)
     
@@ -25,64 +26,91 @@ btnshow.addEventListener('click', () => {
 })
 
 
-
 function Show(){
     document.querySelector(`#listOfNotes`).innerHTML = ``
 
     for(let i=0; i<localStorage.length * 10; i++) {
         if(localStorage.getItem(`title${i}`) != null){
-            var container = document.createElement('div')
-            container.classList.add('container1')
-            
-        
-            
-            //title
-            var titleValue = localStorage.getItem(`title${i}`)    //js string
-            var titleh3 = document.createElement('h3')              //html <h3>
-            titleh3.innerHTML = titleValue                          //js string => html <h3>
-            var titleP = document.createElement('p')              //html <p>
-            titleP.append(titleh3)
-            container.append(titleP)
-            //text
-    
-            var textValue = localStorage.getItem(`text${i}`)
-            var text = document.createElement('text')
-            text.innerHTML = textValue
-            var textP = document.createElement('p')
-            textP.append(text)
-            container.append(textP)
-            //color
-            container.style.backgroundColor = localStorage.getItem(`color${i}`)
-    
-            //date
-            var dateValue = localStorage.getItem(`date${i}`)
-            var date = document.createElement('date')
-            date.innerHTML = dateValue
-            var dateP = document.createElement('p')
-            dateP.append(date)
-            container.append(dateP)
-    
-            
-            var del = document.createElement('button')
-            del.classList.add('btndel')
-            del.innerHTML = 'delete'
-            var delP = document.createElement('p')
-            del.addEventListener('click', () => {
-    
-                localStorage.removeItem(`title${i}`);
-                localStorage.removeItem(`text${i}`);
-                localStorage.removeItem(`color${i}`);
-                localStorage.removeItem(`date${i}`);
-            
-                Show()
-            
-            })
-            delP.append(del)
-            container.append(delP)
-    
-    
-    
-            document.querySelector(`#listOfNotes`).append(container)
+            if(localStorage.getItem(`pin${i}`) === 'true')
+            {
+                createNote(i)
+            }
         }
       }
+    for(let i=0; i<localStorage.length * 10; i++) {
+        if(localStorage.getItem(`title${i}`) != null){
+            if(localStorage.getItem(`pin${i}`) === 'false')
+            {
+                createNote(i)
+            }
+        }
+      }
+
 }
+
+function createNote(i) {
+    var container = document.createElement('div')
+    container.classList.add('container1')
+    //pin
+    var pinChecked = localStorage.getItem(`pin${i}`)
+    var pin = document.createElement("input");
+    pin.setAttribute("type", "checkbox");
+    pin.disabled = true
+    if (pinChecked === 'true')
+    {
+        pin.checked = true
+    }
+    var pinP = document.createElement('p')
+    console.log(pinChecked)
+    pinP.append(pin)
+    container.append(pinP)
+    
+    //title
+    var titleValue = localStorage.getItem(`title${i}`)    //js string
+    var titleh3 = document.createElement('h3')              //html <h3>
+    titleh3.innerHTML = titleValue                          //js string => html <h3>
+    var titleP = document.createElement('p')              //html <p>
+    titleP.append(titleh3)
+    container.append(titleP)
+    //text
+
+    var textValue = localStorage.getItem(`text${i}`)
+    var text = document.createElement('text')
+    text.innerHTML = textValue
+    var textP = document.createElement('p')
+    textP.append(text)
+    container.append(textP)
+    //color
+    container.style.backgroundColor = localStorage.getItem(`color${i}`)
+
+    //date
+    var dateValue = localStorage.getItem(`date${i}`)
+    var date = document.createElement('date')
+    date.innerHTML = dateValue
+    var dateP = document.createElement('p')
+    dateP.append(date)
+    container.append(dateP)
+
+    
+    var del = document.createElement('button')
+    del.classList.add('btndel')
+    del.innerHTML = 'delete'
+    var delP = document.createElement('p')
+    del.addEventListener('click', () => {
+
+        localStorage.removeItem(`title${i}`);
+        localStorage.removeItem(`text${i}`);
+        localStorage.removeItem(`color${i}`);
+        localStorage.removeItem(`date${i}`);
+    
+        Show()
+    
+    })
+    delP.append(del)
+    container.append(delP)
+
+
+
+    document.querySelector(`#listOfNotes`).append(container)
+}
+
