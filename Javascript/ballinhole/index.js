50c5f8c74b3413a9e81165d736c51b5b87804d81
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-11-29 22:26:43 +0100

    Walls

--- Javascript/ballinhole/index.js
+++ Javascript/ballinhole/index.js
@@ -5,8 +5,8 @@ var defaultAlpha
 var defaultBeta
 var defaultGamma
 var animFrame
-var PosY = 90
-var PosX = 90
+var PosY = 150
+var PosX = 300
 
 function onDeviceMove(event) {
 
@@ -17,11 +17,17 @@ function onDeviceMove(event) {
         defaultGamma = event.gamma
     }
 
-
+    
+    // if(ball.style.top < 150 + 'px')
+    // {
+    //     moveY = 0
+    //     ball.style.top = 0 + 'px'
+    // }
     var moveY = (event.beta - defaultBeta) / 19
-    var moveX = (event.gamma - defaultGamma) / 19
+    var moveX = (event.gamma - defaultGamma) / 22
 
     
+
     if(animFrame !== undefined){
         cancelAnimationFrame(animFrame)
     }
@@ -31,23 +37,34 @@ function onDeviceMove(event) {
 
 function animate(moveY, moveX) {
 
+    
     PosY = PosY + moveY
     ball.style.top = PosY + 'px'
 
     PosX = PosX + moveX
     ball.style.left = PosX + 'px'
 
-    // var a = 2
-    // var b = 4
+    if(PosY < 0 - 25)
+    {
+        ball.style.top = -25 + 'px'
+    }
+
+    if(PosY > 700 - 45)
+    {
+        ball.style.top = 655 + 'px'
+    }
+
+    if(PosX < 0 - 60)
+    {
+        ball.style.left = -50 + 'px'
+    }
+
+    if(PosX > 630)
+    {
+        ball.style.left = 630 + 'px'
+    }
 
 
-    // var c = a + b
-
-
-    // a = a + b
-    // c = a
-
-    
     console.log(Date.now())
     //repeat animetion func
     animFrame = requestAnimationFrame(function(){animate(moveY, moveX)})
