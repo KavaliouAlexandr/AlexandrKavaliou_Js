0dcb2daaf6765ff9659c1a5fcb9c8fe27987b9d6
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-11-29 23:50:17 +0100

    Hall

--- Javascript/ballinhole/index.js
+++ Javascript/ballinhole/index.js
@@ -7,9 +7,9 @@ var defaultGamma
 var animFrame
 var PosY = 150
 var PosX = 296
-var HallX = 296
-var HallY = 550 
-var HallWidth = 50
+// var HallX = 296
+// var HallY = 550 
+// var HallWidth = 50
 
 function onDeviceMove(event) {
 
@@ -23,7 +23,12 @@ function onDeviceMove(event) {
     var moveY = (event.beta - defaultBeta) / 19
     var moveX = (event.gamma - defaultGamma) / 22
 
-    
+    if(PosY > 450 && PosX < 550 && PosY < 346 && PosX < 246)
+    {
+        alert("You Win");
+        document.location.reload();
+        clearInterval(interval);
+    }
 
     if(animFrame !== undefined){
         cancelAnimationFrame(animFrame)
@@ -44,36 +49,36 @@ function animate(moveY, moveX) {
     if(PosY < 0 - 25)
     {
         ball.style.top = -25 + 'px'
-        alert("GAME OVER");
-        document.location.reload();
-        clearInterval(interval);
+        // alert("GAME OVER");
+        // document.location.reload();
+        // clearInterval(interval);
     }
 
     if(PosY > 700 - 45)
     {
         ball.style.top = 655 + 'px'
-        alert("GAME OVER");
-        document.location.reload();
-        clearInterval(interval);
+        // alert("GAME OVER");
+        // document.location.reload();
+        // clearInterval(interval);
     }
 
     if(PosX < 0 - 60)
     {
         ball.style.left = -50 + 'px'
-        alert("GAME OVER");
-        document.location.reload();
-        clearInterval(interval);
+        // alert("GAME OVER");
+        // document.location.reload();
+        // clearInterval(interval);
     }
 
     if(PosX > 630)
     {
         ball.style.left = 630 + 'px'
-        alert("GAME OVER");
-        document.location.reload();
-        clearInterval(interval);
+        // alert("GAME OVER");
+        // document.location.reload();
+        // clearInterval(interval);
     }
 
-    if(PosY >= HallY && PosX <= HallX + HallWidth)
+    if(PosX > 250 && PosX < 346 && PosY > 525 && PosY < 600)
     {
         alert("You Win");
         document.location.reload();
