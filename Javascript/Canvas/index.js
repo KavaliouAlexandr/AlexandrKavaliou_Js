783d1430d41ef3f0a7ed21d71ebe1f13deeee99c
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-12-10 23:49:37 +0100

    Ball

--- Javascript/Canvas/index.js
+++ Javascript/Canvas/index.js
@@ -1,12 +1,24 @@
 const canvas = document.querySelector("#balls");
 var ctx = canvas.getContext('2d');
 
-function draw(){
-  ctx.beginPath();
-  ctx.arc(350,350,5,0,Math.PI*2,true); // Внешняя окружность
-  ctx.stroke();
-  ctx.fillStyle = "white";
-  ctx.fill();
-}
+var ball = {
+  x: 100,
+  y: 100,
+  radius: 7,
+  color: 'black',
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius - 2, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fillStyle = 'white';
+    ctx.fill();
+  }
+};
 
-draw()
+ball.draw();
