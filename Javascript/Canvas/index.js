9aa2ad1b11e43f8304b60e08ba93527d5a78e7e6
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-12-12 00:52:33 +0100

    balls

--- Javascript/Canvas/index.js
+++ Javascript/Canvas/index.js
@@ -1,16 +1,23 @@
 const canvas = document.querySelector("#balls");
 var ctx = canvas.getContext('2d');
-var raf;
 
 
-var ball = {
-  x: 200,
-  y: 200,
-  sx: 2,
-  sy: 2,
-  radius: 7,
-  color: 'black',
-  draw: function() {
+class Ball {
+  x
+  y
+  sx
+  sy
+  radius = 7
+  color = 'black'
+
+  constructor (x, y, sx, sy) {
+    this.x = x
+    this.y = y
+    this.sx = sx
+    this.sy = sy
+  }
+
+  draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     ctx.closePath();
@@ -22,29 +29,43 @@ var ball = {
     ctx.closePath();
     ctx.fillStyle = 'white';
     ctx.fill();
+    
   }
 };
 
-function draw() {
+
+// var ball = new Ball(200, 200, 2, 2)
+
+function drawBalls(balls) {
   ctx.clearRect(0,0, canvas.width, canvas.height);
-  ball.draw();
-  ball.x += ball.sx;
-  ball.y += ball.sy;
-
-  if (ball.y + ball.sy > canvas.height || ball.y + ball.sy < 0) {
-    ball.sy = -ball.sy;
+  for (let i = 0; i < balls.length; i++){
+    balls[i].draw();
+    balls[i].x += balls[i].sx;
+    balls[i].y += balls[i].sy;
+    if (balls[i].y + balls[i].sy > canvas.height || balls[i].y + balls[i].sy < 0) {
+      balls[i].sy = -balls[i].sy;
+    }
+    if (balls[i].x + balls[i].sx > canvas.width || balls[i].x + balls[i].sx < 0) {
+      balls[i].sx = -balls[i].sx;
+    }
   }
-  if (ball.x + ball.sx > canvas.width || ball.x + ball.sx < 0) {
-    ball.sx = -ball.sx;
-  }
-
-  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(function(){drawBalls(balls)});
 }
 
+function getRandom(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
-
+var balls = new Array()
 for(let i = 0; i < 10; i++)
 {
+  balls[i] = new Ball(`${i}`, 
+  getRandom(0, canv.width), 
+  getRandom(0, canv.height), 
+  getRandom(-1.5, 1.5), 
+  getRandom(-1.5, 1.5))
 }
 
-window.requestAnimationFrame(draw);
+
+
+window.requestAnimationFrame(function(){drawBalls(balls)});
