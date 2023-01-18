d8e82b1a3cc0f5f22c30d35c9c5f1a891f6e2def
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2023-01-18 21:50:57 +0100

    WeatherApp

--- Javascript/weatherApp/index.js
+++ Javascript/weatherApp/index.js
@@ -2,10 +2,11 @@ const townForm = document.querySelector("#town-form");
 const townInput = document.querySelector("#town-input");
 const townList = document.querySelector("#town-list");
 
-const API_KEY = "fb3e80700711db9ca977701b2e056bf8";
+const API_KEY = "3edbcf6293a9ff2c367754b19c19dfad";
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
 
-let towns = JSON.parse(localStorage.getItem("towns"))
+let towns = new Array();
+towns = JSON.parse(localStorage.getItem("towns"))
 
 townForm.addEventListener("submit", e => {
   e.preventDefault();
@@ -40,46 +41,58 @@ townForm.addEventListener("submit", e => {
       console.error(err);
     });
 });
+
+
+
 // Add town to list
 function addTown(name, temp, humidity, icon) {
-  // Create list item
-  const li = document.createElement("li");
-  li.innerHTML = `
-    <span>${name}</span>
-    <span>${temp}&deg;C</span>
-    <span>${humidity}%</span>
-    <img class="weather-icon" src="${icon}" alt="Weather icon" />
-    <button>&times;</button>
-  `;
-
-  // Add delete button event listener
-  li.querySelector("button").addEventListener("click", e => {
-    e.stopPropagation();
-    deleteTown(li);
-  });
-
-  // Add town to list
-  townList.appendChild(li);
-
   // Add town to local storage
-  towns.push({ name, temp, humidity, icon });
+  let townTemp = [ name, temp, humidity, icon ];
+  if(towns == null){
+    towns = new Array();
+  }
+  towns.push(townTemp);
   localStorage.setItem("towns", JSON.stringify(towns));
+  towns = JSON.parse(localStorage.getItem("towns"))
+
+  Show();
 }
+
+
+
 // Delete town from list
-function deleteTown(li) {
-  // Remove town from list
-  townList.removeChild(li);
+function deleteTown(name) {
 
   // Remove town from local storage
-  towns = towns.filter(town => town.name !== li.firstChild.textContent);
+  towns = towns.filter(town => town[0] !== name);
   localStorage.setItem("towns", JSON.stringify(towns));
+
+  Show();
 }
-//   towns.push({ name, temp, humidity, icon });
-// localStorage.setItem("towns", JSON.stringify(towns));
-// Get towns from local storage and add to list
-if (towns !== null) {
+
+document.querySelector('.towns').addEventListener('click', Show)
+
+function Show(){
+  townList.innerHTML = '';
+
   towns.forEach(town => {
-    const { name, temp, humidity, icon } = town;
-    addTown(name, temp, humidity, icon);
-  });
+    // Create list item
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>${town[0]}</span>
+      <span>${town[1]}&deg;C</span>
+      <span>${town[2]}%</span>
+      <img class="weather-icon" src="${town[3]}" alt="Weather icon" />
+      <button>&times;</button>
+    `;
+
+    // Add delete button event listener
+    li.querySelector("button").addEventListener("click", e => {
+      e.stopPropagation();
+      deleteTown(town[0]);
+    });
+
+    // Add town to list
+    townList.appendChild(li);
+  })
 }
